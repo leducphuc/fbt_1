3417c9cd3df8e8814f8126dd572b303e10ba2945
leducphuc <leducphuc1994@gmail.com>
2016-10-25 11:08:12 +0700

    heroku 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,24 @@ end
 
 Place.create name: "Sa Pa",
   url_image: "http://i.imgur.com/8uOnyCg.jpg",
-  description: "This is Da Nang"
+  description: "This is Sa pa"
 Place.create name: "Moc Chau",
   url_image: "http://i.imgur.com/z5WmdZR.jpg",
-  description: "This is Da Nang"
+  description: "This is Moc Chau"
 Place.create name: "Ly Son",
   url_image: "http://i.imgur.com/gMQP6K6.jpg",
-  description: "This is Da Nang"
+  description: "This is Ly Son"
 Place.create name: "Ha Long",
   url_image: "http://i.imgur.com/sNJSrQv.jpg",
-  description: "This is Da Nang"
+  description: "This is Ha Long"
 Place.create name: "Ba Vi",
   url_image: "http://i.imgur.com/TBfgEWd.jpg",
-  description: "This is Da Nang"
+  description: "This is Ba vi"
 Place.create name: "Hue",
   url_image: "http://i.imgur.com/p7oFjhV.jpg",
-  description: "This is Da Nang"
+  description: "This is Hue"
 Place.create name: "Nha Trang",
   url_image: "http://i.imgur.com/07Iz3jH.jpg",
-  description: "This is Da Nang"
+  description: "This is Nha Trang"
 Category.create name: "Du lich bien", cover: "http://i.imgur.com/3sUOjfH.jpg"
 Category.create name: "Du lich sinh thai", cover: "http://i.imgur.com/AkLaFYY.jpg"

9f16ac521d20619756d20a3ad4cb426ffd0f9bd4
leducphuc <leducphuc1994@gmail.com>
2016-10-25 11:16:23 +0700

    fix paypal

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,7 @@ class Activity < ApplicationRecord
   belongs_to :activable, polymorphic: true, optional: true
 
   scope :my_activity, -> user_id{
-    where("activities.tag_user_id = ?", user_id)}
+    where("tag_user_id = ?", user_id)}
 
   scope :not_seen, -> {where seen: false}
 end
